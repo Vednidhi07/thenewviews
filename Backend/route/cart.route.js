69e603764c85a5861ca5aba2839b6b424321ee75
Vednidhi Kumar <118989492+Vednidhi07@users.noreply.github.com>
2024-07-14 17:47:59 +0530

    Delete Backend directory

--- Backend/route/cart.route.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { addtocart } from "../controller/cart.controller.js";
-const router = express.Router();
-
-router.post("/cart", addtocart);
-
-export default router;
